8479c65edeb713dd32f5ac56871f7832aa927780
Ilham-Nur <ilonoer26@gmail.com>
2025-06-21 04:24:33 +0700

    push api unutk history

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\HistoryController;
 
 Route::get('/tes', function () {
     return response()->json(['message' => 'API works']);
@@ -15,4 +16,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::middleware('auth:sanctum')->post('/change-password', [AuthController::class, 'changePassword']);
+Route::middleware('auth:sanctum')->get('/history/{id}', [HistoryController::class, 'show']);
 
